93bb5f060b84e4dda52089f154b9f49de641cf17
Brice Christian <bricechristian93@gmail.com>
2023-04-03 09:33:40 -0400

    Added photography pages + global utilities

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,17 @@ import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/edge';
 import sanity from "astro-sanity";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-    integrations: [sanity({
-        projectId: 'h890ay2m',
-        dataset: 'production',
-        apiVersion: '2023-02-08',
-        useCdn: false,
-      })],
-      output: 'server',
-      adapter: vercel()
+  site: 'https://jeangillesphotography.vercel.app',
+  integrations: [sanity({
+    projectId: 'h890ay2m',
+    dataset: 'production',
+    apiVersion: '2023-02-08',
+    useCdn: false
+  }), sitemap()],
+  output: 'server',
+  adapter: vercel()
 });
