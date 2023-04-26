c158f625c00e062e0474889b51d4d3f47f916ff5
Brice Christian <bricechristian93@gmail.com>
2023-04-25 21:50:02 -0400

    Removed admin route for now

--- src/pages/admin/static/index-c20e30e3-858e5e3d.js
+++
@@ -1,1 +0,0 @@
-import{DocumentPaneProvider as m,DocumentPane as a}from"./index-63f13ccc-475cc389.js";import"./sanity-d10f4100.js";import"./index-a69ea6d1.js";export{m as DocumentPaneProvider,a as default};
